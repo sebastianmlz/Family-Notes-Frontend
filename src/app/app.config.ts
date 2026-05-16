f4ef0927b679510d24ff46562791a2531fbcb752
sebastianmlz_07 <sebamendex11@gmail.com>
2026-05-16 13:29:26 -0400

    feat: add pnpm workspace configuration and integrate PrimeNG with dark mode toggle
    - Created a new pnpm workspace configuration file to allow builds for specific packages.
    - Updated app configuration to include PrimeNG with a custom theme and dark mode support.
    - Simplified app HTML structure and removed placeholder content, adding a dark mode toggle button.
    - Enhanced app component to handle dark mode toggling via a method.
    - Imported Tailwind CSS and PrimeUI styles for improved styling capabilities.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,26 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(), 
+    provideRouter(routes),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: '.my-app-dark',
+          cssLayer: {
+            name: 'primeng',
+            order: 'theme, base, primeng'
+          }
+        }
+      }
+    })
+  ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('family-notes-frontend');
+
+  toggleDarkMode() {
+    // Buscamos la etiqueta <html>
+    const htmlElement = document.querySelector('html');
+    if (htmlElement) {
+      // La función 'toggle' pone la clase si no existe, y la quita si ya existe
+      htmlElement.classList.toggle('my-app-dark');
+    }
+  }
 }
+
+
